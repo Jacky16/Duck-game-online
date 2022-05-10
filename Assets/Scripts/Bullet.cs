c6a5ee3b02b3b127c2e3405a18d2586e53859f37
Adria Martinez <adriamartinez94@gmail.com>
2022-05-10 14:23:33 +0200

    Movimiento basico con salto y disparo con los assets implementados

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,22 +5,17 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 10f;
-    private Rigidbody2D rb;
     PhotonView photonView;
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
         photonView = GetComponent<PhotonView>();
     }
-    private void Start()
-    {
-        rb.velocity = new Vector2(speed, 0f);
-    }
+  
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.GetComponent<PlayerController>().Damage();
+        if(collision.CompareTag("Player"))
+            collision.GetComponent<PlayerController>().Damage();
         
         photonView.RPC("NetworkDestroy", RpcTarget.All);
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,23 @@ public class PlayerController : MonoBehaviourPun, IPunObservable
     [Header("Stats")]
     [SerializeField] float speed;
     [SerializeField] float jumpForce;
+    
+    [Header("Ground Settins")]
+    [SerializeField] float groundCheckDistance;
+    [SerializeField] LayerMask whatIsGround;
+    bool isGrounded;
+
+    [Header("Shoot")]
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform bulletSpawn;
+    [SerializeField] float shootForce;
 
     Rigidbody2D rb2d;
     Vector2 axis;
     bool isFlipped;
 
     PhotonView photoView;
+    Animator anim;
     struct EnemyTransform
     {
         public Vector3 position;
@@ -25,12 +36,16 @@ public class PlayerController : MonoBehaviourPun, IPunObservable
     {
         photoView = GetComponent<PhotonView>();
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
         PhotonNetwork.SendRate = 50;
         PhotonNetwork.SerializationRate = 50;
     }
     private void Update()
     {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isMoving", axis.x != 0);
         if (photoView.IsMine)
         {
             CheckInputs();
@@ -42,17 +57,16 @@ public class PlayerController : MonoBehaviourPun, IPunObservable
         
     }
 
-    
-
     private void FixedUpdate()
     {     
         rb2d.velocity = (new Vector2(axis.x * speed, rb2d.velocity.y));
-                
+             
     }
 
     void CheckInputs()
     {
         axis.x = Input.GetAxis("Horizontal");
+        
         if (axis.x < 0 && !isFlipped)
         {
             Flip();
@@ -62,7 +76,7 @@ public class PlayerController : MonoBehaviourPun, IPunObservable
             Flip();
         }
             //Jump
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
         }
@@ -85,7 +99,13 @@ public class PlayerController : MonoBehaviourPun, IPunObservable
     }
     void Shoot()
     {
-        PhotonNetwork.Instantiate("Bullet", transform.position + new Vector3(1,0), Quaternion.identity);
+        GameObject bullet;
+        
+        if(PhotonNetwork.CurrentRoom != null)
+        {
+            bullet = PhotonNetwork.Instantiate(bulletPrefab.name, bulletSpawn.position, transform.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.right * shootForce;
+        }
     }
     private void SmoothReplicate()
     {
@@ -117,5 +137,9 @@ public class PlayerController : MonoBehaviourPun, IPunObservable
             enemyTransform.position = (Vector3) stream.ReceiveNext();
             enemyTransform.rotation = (Quaternion) stream.ReceiveNext();
         }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance, transform.position.z));
     }
 }
